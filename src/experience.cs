0b38bb2746170bca4ea75eb0f4091092d27c4845
Darshan Mistry <mistrydarshan711@gmail.com>
2025-05-08 21:32:30 +0530

    Create experience.cs

---
+++ src/experience.cs
@@ -0,0 +1,38 @@
+.experience_heading{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-weight: 2;
+    font-style: bold;
+    font-size: 220px;
+    color: rgb(235, 235, 235);
+    text-align: center;
+}
+
+.experience_card{
+   margin-right:20%;
+   margin-left:25%;
+}
+
+.company_name{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: #333333;
+    font-size: 50px;
+}
+
+.role{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: #333333;
+    font-size: 18px;}
+
+li{
+ color: #333333;
+    font-size: 20px;
+    padding: 10px;   
+}
+
+.line-experience{
+    margin-top: 2%;
+    width: 60%;
+    border: 1px solid black;
+    border-radius: 4px;
+
+}
