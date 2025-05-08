5977d9b9b9732111161f19435127a8f0909b98f9
Darshan Mistry <mistrydarshan711@gmail.com>
2025-05-08 21:36:00 +0530

    Create project_heading.js

---
+++ src/project_heading.js
@@ -0,0 +1,13 @@
+import "./about.css"
+import ScrollAnimation from 'react-animate-on-scroll';
+
+function Project_Heading(props) {
+    return (
+        <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+        <h1 class="experience_heading">PROJECTS</h1>
+        </ScrollAnimation>
+                
+    );
+}
+
+export default Project_Heading;
